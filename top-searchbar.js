21ba38bdbf9c9e591cc416a5cc4935aa67c6882f
Gabriel Zanella <gabrieldoissteam@hotmail.com>
2023-10-26 09:27:33 -0300

    Criado filtro para a lista da barra de pesquisa conforme digitado no input

--- top-searchbar.js
+++ top-searchbar.js
@@ -24,6 +24,9 @@
             const productauthor = document.createElement("p");
             productauthor.textContent = product.author;
 
+            const productBrand = document.createElement("p");
+            productBrand.textContent = product.brand;
+
             const productLink = document.createElement("a");
             productLink.href = product.link;
             productLink.textContent = "Ver na Amazon";
@@ -34,18 +37,40 @@
             productInfoLinkDiv.appendChild(productInfoDiv);
             productInfoDiv.appendChild(productTitle);
             productInfoDiv.appendChild(productauthor);
-            productInfoDiv.appendChild(productauthor);
+            productInfoDiv.appendChild(productBrand);
             productInfoLinkDiv.appendChild(productLink);
 
             return productDiv;
         }
         
-    // Adicione produtos à galeria
-    const searchBarItems = document.querySelector("#searched-list");
-        
-    products.forEach(product => {
-        if( product.topSeller && product.itemType === "book"){
-        const productElement = createProductElement(product);
-        searchBarItems.appendChild(productElement);
-        }
+    // Função para atualizar a lista de produtos com base na consulta
+    function updateProductList(query) {
+        const searchBarItems = document.querySelector("#searched-list");
+        searchBarItems.innerHTML = ""; // Limpe a lista atual
+    
+        products.forEach(product => {
+            if (product && product.topSeller) { // Verifica se o objeto product é definido
+                const productInfo = [
+                    product.title,
+                    product.author,
+                    product.brand
+                ].map(info => info ? info.toLowerCase() : "").join(" "); // Verifica se os campos estão definidos
+    
+                if (productInfo.includes(query)) {
+                    const productElement = createProductElement(product);
+                    searchBarItems.appendChild(productElement);
+                }
+            }
+        });
+    }
+
+    // Adicione um ouvinte de eventos ao campo de entrada
+    const searchInput = document.querySelector("#search-bar-input");
+
+    searchInput.addEventListener("input", function() {
+        const query = searchInput.value.toLowerCase(); // Obtenha o valor do campo de entrada e converta para minúsculas
+        updateProductList(query);
     });
+
+    // Inicialize a lista com todos os produtos topSeller
+    updateProductList("");
