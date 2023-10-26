d366cb1f6d6a556cc7f124db0d24a89e4642c71a
Gabriel Zanella <gabrieldoissteam@hotmail.com>
2023-10-25 22:29:59 -0300

    Arrumado bug de click nos links na lista da barra de busca

--- top-search-list.js
+++ top-search-list.js
@@ -1,23 +1,43 @@
-  // Obtenha o elemento de entrada e a lista
-  const searchBarInput = document.getElementById('search-bar-input');
-  const searchedList = document.getElementById('searched-list');
+// Obtenha o elemento de entrada e a lista
+const searchBarInput = document.getElementById('search-bar-input');
+const searchedList = document.getElementById('searched-list');
 
-  // Adicione um ouvinte de evento de foco (focus) ao campo de entrada
-  searchBarInput.addEventListener('focus', updateVisibility);
-  
-  // Adicione um ouvinte de evento de desfoco (blur) ao campo de entrada
-  searchBarInput.addEventListener('blur', updateVisibility);
-  
-  // Adicione um ouvinte de evento de input ao campo de entrada
-  searchBarInput.addEventListener('input', updateVisibility);
+// Mantenha a lista visível quando o campo de entrada é clicado
+searchBarInput.addEventListener('click', (event) => {
+  updateVisibility();
+  event.stopPropagation();
+});
 
-  function updateVisibility() {
-    // Verifique se o campo de entrada tem conteúdo e está focado
-    if (searchBarInput.value.trim() !== '' && searchBarInput === document.activeElement) {
-      // Se houver conteúdo e o campo de entrada estiver focado, mostre a lista
-      searchedList.style.display = 'block';
-    } else {
-      // Caso contrário, oculte a lista
-      searchedList.style.display = 'none';
-    }
+// Adicione um ouvinte de evento de clique no documento (body)
+document.addEventListener('click', (event) => {
+  if (event.target !== searchBarInput && event.target !== searchedList) {
+    // Se o clique não foi no campo de entrada nem na lista, oculte a lista
+    searchedList.style.display = 'none';
   }
+});
+
+// Adicione um ouvinte de evento de foco (focus) ao campo de entrada
+searchBarInput.addEventListener('focus', () => {
+  updateVisibility();
+});
+
+// Adicione um ouvinte de evento de input ao campo de entrada
+searchBarInput.addEventListener('input', () => {
+  updateVisibility();
+});
+
+// Adicione um ouvinte de evento de clique à lista para mantê-la visível
+searchedList.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+
+function updateVisibility() {
+  // Verifique se o campo de entrada está focado e tem um valor
+  if (searchBarInput === document.activeElement && searchBarInput.value.trim() !== '') {
+    // Se o campo de entrada estiver focado e tiver conteúdo, mostre a lista
+    searchedList.style.display = 'block';
+  } else {
+    // Caso contrário, oculte a lista
+    searchedList.style.display = 'none';
+  }
+}
