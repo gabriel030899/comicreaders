bd92d1b6558c7a57ce8ee261388b65766c46caad
Gabriel Zanella <gabrieldoissteam@hotmail.com>
2023-11-04 14:45:35 -0300

    criado links de suporte e atualizado categorias especiais

--- special-categories-list.js
+++ special-categories-list.js
@@ -1,72 +1,84 @@
 export const specialCategories = [
     {
         title: "Brazuca",
+        name: "Brazuca",
         image: "",
         link: "",
         tooltip: "Para você, amante dos autores brasileiros."
     },
     {
         title: "Busque seus lencinhos",
+        name: "Busque seus lencinhos",
         image: "",
         link: "",
         tooltip: "Histórias para você se emocionar."
     },
     {
         title: "Mundo Mágico",
+        name: "Mundo Mágico",
         image: "",
         link: "",
         tooltip: "Onde fantasia, magia e aventuras se tornam realidade."
     },
     {
         title: "Mistérios Urbanos",
+        name: "Mistérios Urbanos",
         image: "",
         link: "",
         tooltip: "Para quem gosta de investigar mistérios e assassinatos."
     },
     {
         title: "Girassóis e Girassóis",
+        name: "Girassóis e Girassóis",
         image: "",
         link: "",
         tooltip: "Arte, cultura e a vida contemporânea."
     },
     {
         title: "Reinos Perdidos",
+        name: "Reinos Perdidos",
         image: "",
         link: "",
         tooltip: "Castelos, dragões, guerreiros e espadas unidos uma experiência medieval."
     },
     {
         title: "Noites em Claro",
+        name: "Noites em Claro",
         image: "",
         link: "",
         tooltip: "Suspenses de roer os dedos e passar noites acordado."
     },
     {
         title: "Quando o medo chama",
+        name: "Quando o medo chama",
         image: "",
         link: "",
         tooltip: "Impossível ler de luzes apagadas e dormir com o pé descoberto."
     },
     {
         title: "Arrumando a cama",
+        name: "Arrumando a cama",
         image: "",
         link: "",
         tooltip: "Para você que quer mudar sua vida, comece arrumando sua cama."
     },
     {
         title: "Amor Proibido",
+        name: "Amor Proibido",
         image: "",
         link: "",
         tooltip: "Paixão, amor e sedução. Tudo entre quatro paredes."
     },
     {
         title: "Estradas e Histórias",
+        name: "Estradas e Histórias",
         image: "",
         link: "",
         tooltip: "De aventureiros, para aventureiros."
     },
     {
         title: "Anime-se",
+        name: "Anime-se",
         image: "",
         link: "",
         tooltip: "Piratas se esticando, titãs invadindo cidades e muito mais."
