60f235f018e8bb09d48a2404aad4a83caa4687e1
Gabriel Zanella <gabrieldoissteam@hotmail.com>
2023-11-07 18:06:49 -0300

    criado livros novos

---
+++ generate-products-list.js
@@ -0,0 +1,14 @@
+// Referência ao banco de dados de livros (substitua pelo seu banco de dados real)
+import { products } from './all-products.js';
+
+// Função para extrair e listar os títulos dos produtos
+function listProductTitles() {
+    const titles = products.map(product => product.title);
+    return titles;
+}
+
+// Captura a lista de títulos
+const productTitles = listProductTitles();
+
+// Exibe a lista de títulos no console
+console.log(productTitles);
